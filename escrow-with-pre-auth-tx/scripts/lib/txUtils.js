c47c2ce6fea9d7e034e883ae9b3e93505e2fe904
Sylvain Faucherand <syl.faucherand@gmail.com>
2018-09-04 20:15:07 +0100

    Add ecrow lifecycle using pre-auth transaction

---
+++ escrow-with-pre-auth-tx/scripts/lib/txUtils.js
@@ -0,0 +1,19 @@
+const fs = require('fs').promises
+const { Stellar } = require('./sdk')
+
+const loadTx = async (path) => {
+    const fundsReleaseTx = await fs.readFile(path, { encoding: 'base64' })
+
+    const buffer = Buffer.from(fundsReleaseTx, 'base64')
+
+    const envelope = Stellar.xdr.TransactionEnvelope.fromXDR(buffer, 'base64')
+
+    const transaction = new Stellar.Transaction(envelope)
+
+    return transaction
+}
+
+const saveTx = async (filename, transaction) => 
+    fs.writeFile(filename, transaction.toEnvelope().toXDR('base64'), { encoding: 'base64' })
+
+module.exports = { loadTx, saveTx }
